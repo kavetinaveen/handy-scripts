3462d73ae56ac50ec81eb0bfee7eb2496cb19148
kavetinaveen <kavetinaveen@users.noreply.github.com>
2021-07-05 23:28:04 +0530

    Update random_sol_generator.py

--- random_sol_generator.py
+++ random_sol_generator.py
@@ -18,19 +18,28 @@ def generate_solutions(df, generate_all=False):
     :return sol: list of all possible solutions or sample solutions generated for the given sample
     :return type: list of lists
     '''
-    df['tmp'] = df.apply(lambda x: [x['1'], 'X' if x['X'] else 0, 2 if x['2'] else 0], axis=1)
+    df['tmp'] = df.apply(lambda x: [str(x['1']), 'X' if x['X'] else 0, '2' if x['2'] else 0], axis=1)
     df['tmp'] = df.apply(lambda x: [y for y in x['tmp'] if y != 0], axis=1)
     if generate_all:
-        pos_sol = [[1, 'X', 2]]*len(df)
+        pos_sol = [['1', 'X', '2']]*len(df)
     else:
         pos_sol = df['tmp'].tolist()
     result_list = result['realresult'].tolist()
     sol = []
+    k = 0
     for element in itertools.product(*pos_sol):
-        sol.append(list(element))        
+        sol.append(list(element))
+        count = 0
+        for i, j in zip(result_list, list(element)):
+            if i == j:
+                count += 1
+        if count>11:
+            print(f"{count} choices correct out of 15 at index {k}")
         if list(element) == result_list:
             print("Congratulations!!!")
+        k += 1
     return sol
 
 results_all = pd.DataFrame(generate_solutions(df))
 results_all.to_csv('all_solutions.csv', index=False)
+results_all
