f31414782b035c3e95cb253a05a96a7971df7ceb
kavetinaveen <kavetinaveen@users.noreply.github.com>
2021-07-13 14:39:57 +0530

    Update calc.py

--- calc.py
+++ calc.py
@@ -9,9 +9,11 @@ df['AwayTeam'] = df['AwayTeam'].apply(lambda x: ''.join(e for e in x if e.isalnu
 df['Number_Home'] = None
 df['Win_Home'] = None
 df['WinPercentage_Home'] = None
+df['WinPercentage_Home_3'] = None
 df['Number_Away'] = None
 df['Win_Away'] = None
 df['WinPercentage_Away'] = None
+df['WinPercentage_Away_3'] = None
 for i in range(len(df)):
     date = df.loc[i, 'Date']
     hteam = df.loc[i, 'HomeTeam']
@@ -23,14 +25,18 @@ for i in range(len(df)):
         if len(df_sub_home) > 0:
             df_sub_home['WinningTeam'] = df_sub_home.apply(lambda x: x['HomeTeam'] if x['HomePts'] > x['AwayPts'] else x['AwayTeam'], axis=1)
             df_sub_home['WinningTeam'] = df_sub_home.apply(lambda x: 'Tie' if x['HomePts'] == x['AwayPts'] else x['WinningTeam'], axis=1)
+            df_sub_home_3 = df_sub_home.tail(3)
             df.loc[i, 'Number_Home'] = len(df_sub_home)
             df.loc[i, 'Win_Home'] = len(df_sub_home[df_sub_home['WinningTeam']==hteam])
             df.loc[i, 'WinPercentage_Home'] = len(df_sub_home[df_sub_home['WinningTeam']==hteam])/len(df_sub_home)
+            df.loc[i, 'WinPercentage_Home_3'] = len(df_sub_home_3[df_sub_home_3['WinningTeam']==hteam])/len(df_sub_home_3)
         if len(df_sub_away) > 0:
             df_sub_away['WinningTeam'] = df_sub_away.apply(lambda x: x['HomeTeam'] if x['HomePts'] > x['AwayPts'] else x['AwayTeam'], axis=1)
             df_sub_away['WinningTeam'] = df_sub_away.apply(lambda x: 'Tie' if x['HomePts'] == x['AwayPts'] else x['WinningTeam'], axis=1)
+            df_sub_away_3 = df_sub_away.tail(3)
             df.loc[i, 'Number_Away'] = len(df_sub_away)
             df.loc[i, 'Win_Away'] = len(df_sub_away[df_sub_away['WinningTeam']==ateam])
             df.loc[i, 'WinPercentage_Away'] = len(df_sub_away[df_sub_away['WinningTeam']==ateam])/len(df_sub_away)
+            df.loc[i, 'WinPercentage_Away_3'] = len(df_sub_away_3[df_sub_away_3['WinningTeam']==ateam])/len(df_sub_away_3)
 
 df.to_csv('output.csv', index=False)
