6c4e1cc3c65b1f5f6c878fff7d1efa62d7293f3b
kavetinaveen <kavetinaveen@users.noreply.github.com>
2021-07-13 11:33:00 +0530

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,32 +1,31 @@
 import pandas as pd
 
 df = pd.read_csv('./tuekysuperlig.csv', engine='python')
-df['Date'] = pd.to_datetime(df['Date'])
+df['Date'] = pd.to_datetime(df['Date'], format="%d.%m.%Y")
 df = df.sort_values(['Date']).reset_index(drop=True)
 
-df['HomeTeam'] = df['HomeTeam'].apply(lambda x: ''.join(e for e in x if e.isalnum()))
-df['AwayTeam'] = df['AwayTeam'].apply(lambda x: ''.join(e for e in x if e.isalnum()))
+hTeam = df['HomeTeam'].unique()
+df['Number_Hometeam'] = None
+df['Score_Hometeam'] = None
+df['WinPercentage_Hometeam'] = None
+for h in hTeam:
+    df.loc[df['HomeTeam']==h, 'Number_Hometeam'] = range(len(df[df['HomeTeam']==h]))
+    df.loc[df['HomeTeam']==h, 'Score_Hometeam'] = df[df['HomeTeam']==h].apply(lambda x: 1 if x['HomePts'] > x['AwayPts'] else 0, axis=1)
+    df.loc[df['HomeTeam']==h, 'Score_Hometeam'] = df.loc[df['HomeTeam']==h, 'Score_Hometeam'].cumsum()
+    df.loc[df['HomeTeam']==h, 'Score_Hometeam'] = df.loc[df['HomeTeam']==h, 'Score_Hometeam'].shift()
+df['Score_Hometeam'] = df['Score_Hometeam'].fillna(0)
+df['WinPercentage_Hometeam'] = df['Score_Hometeam']/df['Number_Hometeam'].apply(lambda x: max(x, 1))
 
-df['WinningTeam'] = df.apply(lambda x: x['HomeTeam'] if x['HomePts'] > x['AwayPts'] else x['AwayTeam'], axis=1)
-df['WinningTeam'] = df.apply(lambda x: 'Tie' if x['HomePts'] == x['AwayPts'] else x['WinningTeam'], axis=1)
+aTeam = df['AwayTeam'].unique()
+df['Number_Awayteam'] = None
+df['Score_Awayteam'] = None
+df['WinPercentage_Awayteam'] = None
+for a in aTeam:
+    df.loc[df['AwayTeam']==a, 'Number_Awayteam'] = range(len(df[df['AwayTeam']==a]))
+    df.loc[df['AwayTeam']==a, 'Score_Awayteam'] = df[df['AwayTeam']==a].apply(lambda x: 1 if x['AwayPts'] > x['HomePts'] else 0, axis=1)
+    df.loc[df['AwayTeam']==a, 'Score_Awayteam'] = df.loc[df['AwayTeam']==a, 'Score_Awayteam'].cumsum()
+    df.loc[df['AwayTeam']==a, 'Score_Awayteam'] = df.loc[df['AwayTeam']==a, 'Score_Awayteam'].shift()
+df['Score_Awayteam'] = df['Score_Awayteam'].fillna(0)
+df['WinPercentage_Awayteam'] = df['Score_Awayteam']/df['Number_Awayteam'].apply(lambda x: max(x, 1))
 
-tmp1 = df[['Date', 'HomeTeam', 'WinningTeam']]
-tmp1.columns = ['Date', 'Team', 'WinningTeam']
-tmp2 = df[['Date', 'AwayTeam', 'WinningTeam']]
-tmp2.columns = ['Date', 'Team', 'WinningTeam']
-df_expand = pd.concat([tmp1, tmp2])
-df_expand.sort_values(['Date'], inplace=True)
-
-team = df_expand['Team'].unique()
-df_expand['Number'] = None
-df_expand['Score'] = None
-df_expand['WinPercentage'] = None
-for t in team:
-    df_expand.loc[df_expand['Team']==t, 'Number'] = range(len(df_expand[df_expand['Team']==t]))
-    df_expand.loc[df_expand['Team']==t, 'Score'] = df_expand[df_expand['Team']==t].apply(lambda x: 1 if x['WinningTeam'] == t else 0, axis=1)
-    df_expand.loc[df_expand['Team']==t, 'Score'] = df_expand[df_expand['Team']==t]['Score'].cumsum()
-    df_expand.loc[df_expand['Team']==t, 'Score'] = df_expand[df_expand['Team']==t]['Score'].shift()
-df_expand['Score'] = df_expand['Score'].fillna(0)
-df_expand['WinPercentage'] = df_expand['Score']/df_expand['Number'].apply(lambda x: max(x, 1))
-
-df_expand.to_csv('output.csv', index=False)
+df.to_csv('output.csv', index=False)
